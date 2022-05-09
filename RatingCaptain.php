46e3a24d462e68509f97828db76440961b2ab8bb
ratingcaptain <mbielak@ideashirt.pl>
2022-05-09 09:13:05 +0200

    API changes

--- RatingCaptain.php
+++ RatingCaptain.php
@@ -1,58 +1,75 @@
 <?php
+namespace App\Clients;
+class RatingCaptain
+{
+    protected $websiteToken;
+    protected $order = [];
+    protected $products = [];
+    private $store_url = 'https://ratingcaptain.com/api/emails';
 
-class RatingCaptain{
-    protected $apiKey, $order=array(), $products = array();
-    private $store_url = 'https://ratingcaptain.com/api/emails', $delete_url="https://ratingcaptain.com/api/website_rate/destroy";
-
-    public function __construct($apiKey)
+    public function __construct($websiteToken)
     {
-        $this->apiKey = $apiKey;
+        $this->websiteToken = $websiteToken;
     }
 
-    public function addProduct($id, $name, $price = null, $imageUrl = null, $desc = null){
+    public function addProduct($id, $name, $price = null, $imageUrl = null, $desc = null)
+    {
         array_push($this->products, [
             'id' => $id,
             'name' => $name,
             'price' => $price,
             'imageUrl' => $imageUrl,
-            'desc' => $desc
+            'desc' => $desc,
         ]);
     }
 
-    private function curl($data, $method = 'post', $url){
+    private function curl($data, $method, $url)
+    {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        if($method == 'post'){
+        if ($method == 'post') {
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(['data' => $data]));
         }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/json'
+        ]);
         $response = curl_exec($ch);
         curl_close($ch);
         return $response;
     }
 
-    public function send($data){
-
-        $data['hash'] = $this->apiKey;
-        foreach (['external_id', 'email'] as $field){
-            if(!$this->checkField($data, $field)) return ['errors' => $field.' is required'];
+    public function send($data)
+    {
+        $data['hash'] = $this->websiteToken;
+        foreach (['external_id', 'email'] as $field) {
+            if (! $this->checkField($data, $field)) {
+                return ['errors' => $field.' is required'];
+            }
         }
         $this->order['external_id'] = $data['external_id'];
         $this->order['email'] = $data['email'];
-        if(array_key_exists('send_date', $data)) $this->order['send_date'] = $data['send_date'];
-        if(array_key_exists('send_after', $data)) $this->order['send_after'] = $data['send_after'];
-        if(array_key_exists('name', $data)) $this->order['name'] = $data['name'];
-        if(array_key_exists('surname', $data)) $this->order['surname'] = $data['surname'];
-        $this->order['hash'] = $this->apiKey;
+        if (array_key_exists('send_date', $data)) {
+            $this->order['send_date'] = $data['send_date'];
+        }
+        if (array_key_exists('name', $data)) {
+            $this->order['name'] = $data['name'];
+        }
+        if (array_key_exists('surname', $data)) {
+            $this->order['surname'] = $data['surname'];
+        }
+        $this->order['hash'] = $this->websiteToken;
         $arr = $this->order;
-        if(count($this->products) > 0) $arr['products'] = $this->products;
-        return $this->curl(json_encode($arr), 'post', $this->store_url);
+        if (count($this->products) > 0) {
+            $arr['products'] = $this->products;
+        }
 
+        return $this->curl(json_encode($arr), 'post', $this->store_url);
     }
-    public function deleteOrder($id){
-        return $this->curl(json_encode(['id' => $id, 'hash' => $this->apiKey]), 'post', $this->delete_url);
-    }
-    private function checkField($arr, $field){
+
+
+    private function checkField($arr, $field)
+    {
         return array_key_exists($field, $arr);
     }
 }
